c3585fda7bda12b367969af1572acd9fda078e21
ipek ay <ipek_snp@hotmail.com>
2024-08-12 04:41:19 +0300

    routes  created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import NavBar from "./component/Navbar.js";
+import HomePage from './pages/homepage.js';
+import List from './pages/list.js';
+import Detail from './pages/detail.js'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar />
+
+      <Routes>
+        <Route path="/pages" element={<HomePage />} />
+        <Route path="/pages/list" element={<List />} />
+        <Route path="/pages/detail" element={<Detail />} />
+      </Routes>
+    </>
+
   );
 }
 
